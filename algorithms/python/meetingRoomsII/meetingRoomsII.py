6095eee7af5271153601d8edd8d4de996cfccddb
MichelleZ <zhangdaxiaomiao@163.com>
2021-01-29 10:59:44 +0800

    253

--- algorithms/python/meetingRoomsII/meetingRoomsII.py
+++ algorithms/python/meetingRoomsII/meetingRoomsII.py
@@ -8,4 +8,12 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 	    from collections import heapq
+		intervals.sort(key = lambda x: x[0})
+        heap = []
+		for it in intervals:
+		if heap and it[0] >= heap[0]:
+		    heapq.heapreplace(heap, it[2])
+		else:
+		    heapq.heappush(heap, it[2])
+	    return len(heapq)
 
