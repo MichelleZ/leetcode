f65e857009f2f8f4cdae491d172ea5e6d7804522
MichelleZ <zhangdaxiaomiao@163.com>
2021-05-07 21:37:39 +0800

    format

--- algorithms/python/numberofStudentsDoingHomeworkataGivenTime/numberofStudentsDoingHomeworkataGivenTime.py
+++ algorithms/python/numberofStudentsDoingHomeworkataGivenTime/numberofStudentsDoingHomeworkataGivenTime.py
@@ -12,3 +12,13 @@ class Solution:
             if startTime[i] <= queryTime and queryTime <= endTime[i]:
                 res += 1
         return res
+
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
